d2db4624f26b7bf3c76485712f16465a6f2f36d0
BrianWeiHaoMa <brian.ma@nexusenergycanada.com>
2024-06-17 16:52:04 -0400

    added some extra functionality and reformatted code

--- src/csvcmp.py
+++ src/csvcmp.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from collections.abc import Callable
 import json
 import os
 
@@ -8,40 +8,62 @@ import pandas as pd
 class CsvCmp:
     def __init__(
             self, 
-            input_dir_path: str,
-            data_save_dir_path: str = "./csvcmp-data/"
+            input_dir_path: str = "./",
+            data_save_dir_path: str = "./csvcmp-data/",
         ):
         self.input_dir_path = input_dir_path
         self.data_save_dir_path = data_save_dir_path
 
     def compare_row_existence(
             self, 
-            left_input: List[str | pd.DataFrame] | str | pd.DataFrame,
-            right_input: List[str | pd.DataFrame] | str | pd.DataFrame,
+            left_input: list[str | pd.DataFrame] | str | pd.DataFrame,
+            right_input: list[str | pd.DataFrame] | str | pd.DataFrame,
+            left_trans_funcs: list[Callable[[pd.DataFrame], pd.DataFrame]] | None = None,
+            right_trans_funcs: list[Callable[[pd.DataFrame], pd.DataFrame]] | None = None,
+            data_save_file_extensions: list[str] | None = None,
             ignore_null_rows: bool = False,
             match_rows: bool = True,
-            use_input_dir_path: bool = True
-        ):
+            use_input_dir_path: bool = True,
+            output_transformed_rows: bool = False,
+        ): 
+        print(
+            self._get_function_info_string(
+                self.compare_row_existence.__name__, 
+                locals(),
+            )
+        )
+
         if type(left_input) != list:
             left_input = [left_input]
             right_input = [right_input]
 
         if len(left_input) != len(right_input):
             raise ValueError("The number of elements in left_input and right_input should be the same")
+        
+        if data_save_file_extensions is None:
+            data_save_file_extensions = ["csv"] * len(left_input)
+        elif len(data_save_file_extensions) != len(left_input):
+            raise ValueError("The number of elements in data_save_file_extensions should be the same as the number of elements in left_input")
+        
+        if left_trans_funcs is None:
+            left_trans_funcs = [lambda x: x] * len(left_input)
+        elif len(left_trans_funcs) != len(left_input):
+            raise ValueError("The number of elements in left_trans_funcs should be the same as the number of elements in left_input")
+        
+        if right_trans_funcs is None:
+            right_trans_funcs = [lambda x: x] * len(right_input)
+        elif len(right_trans_funcs) != len(right_input):
+            raise ValueError("The number of elements in right_trans_funcs should be the same as the number of elements in right_input")
 
-        all_locals = locals()
-        all_locals.pop("self")
-        result_string = self._get_function_info_string(self.compare_row_existence.__name__, all_locals)
-
-        left_names = [
-            self._get_file_name(_) if type(_) == str 
-            else "df_" + str(_.shape()) 
-            for _ in left_input
+        left_file_names = [
+            self._get_file_name(input_) if type(input_) == str 
+            else f"df_{i}_{input_.shape}"
+            for i, input_ in enumerate(left_input)
         ]
-        right_names = [
-            self._get_file_name(_) if type(_) == str 
-            else "df_" + str(_.shape()) 
-            for _ in right_input
+        right_file_names = [
+            self._get_file_name(input_) if type(input_) == str 
+            else f"df_{i}_{input_.shape}"
+            for i, input_ in enumerate(right_input)
         ]
 
         left_dfs, right_dfs = self._change_inputs_to_dfs(
@@ -51,110 +73,161 @@ class CsvCmp:
             ignore_null_rows=ignore_null_rows,
         )
 
-        def _only_in_format(name: str, df: pd.DataFrame) -> str:
+        def _only_in_format(
+                name: str, 
+                df: pd.DataFrame,
+            ) -> str:
             return f"Only in {name} {df.shape}:\n{df.head(20)}\n\n"
         
-        def _get_left_and_right_only(left_df: pd.DataFrame, right_df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        def _get_left_and_right_only_indices(
+                left_df: pd.DataFrame, 
+                right_df: pd.DataFrame,
+            ) -> tuple[pd.Series, pd.Series]:
             columns = list(left_df.columns)
             left_df_tmp = left_df.reset_index(names="_left_index")
             right_df_tmp = right_df.reset_index(names="_right_index")
-            outer_df = pd.merge(left_df_tmp, right_df_tmp, how="outer", on=list(columns), indicator=True)
+            outer_df = pd.merge(
+                left_df_tmp, 
+                right_df_tmp, 
+                how="outer", 
+                on=columns, 
+                indicator=True,
+            )
 
-            left_index_series = outer_df[outer_df["_merge"] == "left_only"]["_left_index"]
-            right_index_series = outer_df[outer_df["_merge"] == "right_only"]["_right_index"]
-            left_only_final = left_df.iloc[left_index_series]
-            right_only_final = right_df.iloc[right_index_series]
+            left_index_series = outer_df[
+                outer_df["_merge"] == "left_only"
+            ]["_left_index"]
+            right_index_series = outer_df[
+                outer_df["_merge"] == "right_only"
+            ]["_right_index"]
+            return left_index_series, right_index_series
 
-            return left_only_final, right_only_final
-
-        for left_df, right_df, left_name, right_name, i in zip(
-            left_dfs, 
-            right_dfs, 
-            left_names, 
-            right_names, 
-            range(len(left_dfs))
+        for i, (
+            left_df, 
+            right_df, 
+            left_trans_func,
+            # right_trans_func,
+            left_file_name, 
+            right_file_name, 
+            data_save_file_extension,
+        ) in enumerate(
+            zip(
+                left_dfs, 
+                right_dfs, 
+                left_trans_funcs,
+                # right_trans_funcs,
+                left_file_names, 
+                right_file_names, 
+                data_save_file_extensions,
+            )
         ):
-            result_string += f"For index {i}\n\n"
-            
-            # Assume that the columns of left_df and right_df are exactly the same.
-            if match_rows:
-                left_value_counts = left_df.value_counts()
-                right_value_counts = right_df.value_counts()
+            left_data_save_file_name = (
+                f"only_in_" +
+                self._get_file_name_no_extension(left_file_name)
+                + f".{data_save_file_extension}"
+            )
+            right_data_save_file_name = (
+                f"only_in_" +
+                self._get_file_name_no_extension(right_file_name)
+                + f".{data_save_file_extension}"
+            )
 
-                shared = left_value_counts.index.intersection(right_value_counts.index)
+        #     print(f"For index {i}\n")
 
-                shared_left_sub_right = left_value_counts[shared] - right_value_counts[shared]
-                shared_right_sub_left = right_value_counts[shared] - left_value_counts[shared]
+        #     # Assume that the columns of left_df and right_df are exactly the same.
+        #     if match_rows:
+        #         left_value_counts = left_df.value_counts()
+        #         right_value_counts = right_df.value_counts()
 
-                left_extra = shared_left_sub_right[shared_left_sub_right > 0]
-                right_extra = shared_right_sub_left[shared_right_sub_left > 0]
+        #         shared = left_value_counts.index.intersection(right_value_counts.index)
+
+        #         shared_left_sub_right = left_value_counts[shared] - right_value_counts[shared]
+        #         shared_right_sub_left = right_value_counts[shared] - left_value_counts[shared]
+
+        #         left_extra = shared_left_sub_right[shared_left_sub_right > 0]
+        #         right_extra = shared_right_sub_left[shared_right_sub_left > 0]
                 
-                left_to_add = []
-                for ind, count in list(pd.DataFrame(left_extra).itertuples(index=True)):
-                    left_to_add.append(left_df[left_df.isin(ind)].dropna().head(count))
+        #         left_to_add_indices = []
+        #         for ind, count in list(pd.DataFrame(left_extra).itertuples(index=True)):
+        #             left_to_add_indices.append(left_df[left_df.isin(ind)].dropna().head(count))
 
-                right_to_add = []
-                for ind, count in list(pd.DataFrame(right_extra).itertuples(index=True)):
-                    right_to_add.append(right_df[right_df.isin(ind)].dropna().head(count))
+        #         right_to_add_indices = []
+        #         for ind, count in list(pd.DataFrame(right_extra).itertuples(index=True)):
+        #             right_to_add_indices.append(right_df[right_df.isin(ind)].dropna().head(count))
 
-                left_only, right_only = _get_left_and_right_only(left_df, right_df)
+        #         left_only, right_only = _get_left_and_right_only(left_df, right_df)
 
-                left_only_final = pd.concat([left_only] + left_to_add).sort_index()
-                right_only_final = pd.concat([right_only] + right_to_add).sort_index()
-                print(right_only_final)
-                exit(1)
-            else:
-                left_only_final, right_only_final = _get_left_and_right_only(left_df, right_df)
+        #         left_only_final = pd.concat([left_only] + left_to_add_indices).sort_index()
+        #         right_only_final = pd.concat([right_only] + right_to_add_indices).sort_index()
+        #     else:
+        #         left_only_final, right_only_final = _get_left_and_right_only(left_df, right_df)
 
-            result_string += _only_in_format(left_name, left_only_final)
-            result_string += _only_in_format(right_name, right_only_final)
+        #     result_string += _only_in_format(left_file_name, left_only_final)
+        #     result_string += _only_in_format(right_file_name, right_only_final)
             
-            result_string += "\n"
-            # self._output_to_file(
-            #     file_sub_path=f"{self.compare_row_existence.__name__}/{left_name}_only.csv", 
-            #     df=left_only_final, 
-            #     data_save_dir_path=self.data_save_dir_path
-            # )
-            # self._output_to_file(
-            #     file_sub_path=f"{self.compare_row_existence.__name__}/{right_name}_only.csv", 
-            #     df=right_only_final, 
-            #     data_save_dir_path=self.data_save_dir_path
-            # )
-        print(result_string)
+        #     result_string += "\n"
 
-    def _get_function_info_string(
+        #     output_dir_path = self._make_output_dir(self.compare_row_existence.__name__)
+        #     self._output_to_file(
+        #         output_dir_path=output_dir_path,
+        #         file_name=left_data_save_file_name,
+        #         df=left_only_final, 
+        #     )
+        #     self._output_to_file(
+        #         output_dir_path=output_dir_path,
+        #         file_name=right_data_save_file_name,
+        #         df=right_only_final, 
+        #     )
+        # print(result_string)
+
+    def _get_file_extension(
             self, 
-            func_name: str, 
-            all_locals: str
+            file_path: str,
         ) -> str:
-        return f"{func_name}()\n{json.dumps(all_locals, indent=4)}\n"
+        file_name = os.path.basename(file_path)
+        arr = file_name.split(".")
+        if len(arr) <= 1 or arr[-1] == "":
+            raise ValueError(f"File path {file_path} does not have an extension")
+        return arr[-1]
+    
+    def _get_file_name_no_extension(
+            self, 
+            file_path: str,
+        ) -> str:
+        file_name = os.path.basename(file_path)
+        file_name_parts = file_name.split(".")[:-1]
+        return ".".join(file_name_parts)
     
     def _get_file_name(
             self, 
             file_path: str
         ) -> str:
-        return file_path.split("/")[-1]
+        file_name = os.path.basename(file_path)
+        return file_name
     
-    def _remove_file_extension(
-            self, 
-            file_name: str
-        ) -> str:
-        return file_name.split(".")[0]
-      
-    def _output_to_file(
-            self, 
-            file_sub_path: str, 
-            df: pd.DataFrame,
-            data_save_dir_path: str,
-            add_timestamp: bool = True
-        ) -> None:
+    def _make_output_dir(
+        self,
+        save_sub_folder: str,
+        add_timestamp: bool = True,
+    ) -> str:
         if add_timestamp:
-            file_sub_path = file_sub_path.split(".")
-            file_sub_path = file_sub_path[0] + "_" + str(pd.Timestamp.now()) + "." + file_sub_path[1]
+            data_save_dir_path = os.path.join(
+                self.data_save_dir_path, 
+                f"{save_sub_folder}_{pd.Timestamp.now().strftime('%Y-%m-%d-%H%M%S')}",
+            )
+        else:
+            data_save_dir_path = os.path.join(self.data_save_dir_path, save_sub_folder)
 
         os.makedirs(data_save_dir_path, exist_ok=True)
-
-        full_file_path = os.path.join(data_save_dir_path, file_sub_path)
+        return data_save_dir_path
+        
+    def _output_to_file(
+        self, 
+        output_dir_path: str,
+        file_name: str,
+        df: pd.DataFrame,
+    ) -> None:
+        full_file_path = os.path.join(output_dir_path, file_name)
         if full_file_path.endswith(".csv"):
             df.to_csv(full_file_path, index=False)
         elif full_file_path.endswith(".xlsx"):
@@ -162,13 +235,27 @@ class CsvCmp:
         else:
             raise ValueError(f"File type not supported: {full_file_path}")
 
+    def _get_function_info_string(
+            self, 
+            func_name: str, 
+            all_locals: str
+        ) -> str:
+        all_locals.pop("self")
+        return f"{func_name}()\n{json.dumps(all_locals, indent=4)}\n"
+    
+    def _remove_file_extension(
+            self, 
+            file_name: str
+        ) -> str:
+        return file_name.split(".")[0]
+
     def _change_inputs_to_dfs(
             self, 
-            first_input: List[str | pd.DataFrame], 
+            first_input: list[str | pd.DataFrame], 
             use_input_dir_path: bool,
             ignore_null_rows: bool,
-            **kwargs
-        ) -> Tuple[List[pd.DataFrame], ...]:
+            **kwargs,
+        ) -> tuple[list[pd.DataFrame], ...]:
         all_inputs = ([first_input] + [_ for _ in kwargs.values()]).copy()
 
         for input_ in all_inputs:
